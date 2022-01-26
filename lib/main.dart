383e802013f9e60c890e9a1fef878a466b411e50
unknown <contactabisoye@gmail.com>
2022-01-26 00:07:58 -0800

    my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    backgroundColor: Colors.black,
+    appBar: AppBar(
+    centerTitle: true,
+    title: Text("Magic 8-ball",),
+    backgroundColor: Colors.black,
+    ),
+
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballface = 1;
+
+  void ChangeBallFace(){
+    setState(() {
+       ballface = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: (){
+           ChangeBallFace();
+        },
+        child: Image.asset("images/ball$ballface.png"),
+      ),
+    );
+  }
+}
+
